3c3c5a495606c3c3e641d4a65097931044091eed
J0ssy <j.g.basterrica@gmail.com>
2018-01-11 12:18:21 -0300

    Unificando los cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,106 @@ $(document).ready(() => {
   $('.modal').modal();
   $('.tooltipped').tooltip({delay: 30});
   $('ul.tabs').tabs('select_tab', 'tab_id');
+  observer();
   $('.carousel.carousel-slider').carousel({fullWidth: true});
   $('.carousel').carousel({
         dist: -50,
         padding: 100,
   });
+
   imgsCarrousel();
   
-  
+  /*
+  *Firebase: Registrar Usuario
+  */ 
+  $('#signup').click(function() {
+    var email = $('#email').val();
+    var pass = $('#password').val();
+    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert('Debe ingresar un texto válido');
+    // ...
+    });
+  });
 
+  /*
+  *Firebase: Iniciar Sesión Usuario
+  */ 
+  $('#login').click(function() {
+    var email2 = $('#email2').val();
+    var pass2 = $('#password2').val();
+    firebase.auth().signInWithEmailAndPassword(email2, pass2).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert('Correo electrónico y/o Contraseña inválidos');
+    // ...
+    });
+  });
+
+  /*
+  *Firebase: Observador
+  */
+  function observer() {
+    firebase.auth().onAuthStateChanged(function(user) {
+       if (user) {
+      // User is signed in.
+       console.log('logeado');
+       gotToProfile();
+       var displayName = user.displayName;
+       var email = user.email;
+       var emailVerified = user.emailVerified;
+       var photoURL = user.photoURL;
+       var isAnonymous = user.isAnonymous;
+       var uid = user.uid;
+       var providerData = user.providerData;
+
+       // ...
+       } else { 
+       // User is signed out.
+       // ...
+
+      }
+    });
+  }
+
+  /*
+  *Firebase: Ir a el Perfil (Luego de registrarse por primera vez)
+  */
+  function gotToProfile() {
+    window.location = ("editProfile.html");
+  }
+
+  /*
+  *Firebase: Ir al bsucador (Luego de iniciar sesión)
+  */
+  function gotToProfile() {
+    window.location = ("editProfile.html");
+  }
+  
+  /*
+  *Cambiando clases de las Tabs
+  */
+  $('.login-btn').click(function() {
+    $('#openSignUp').removeClass('active');
+    $('#openLogin').addClass('active');
+    
+  });
+
+  $('.resgister-btn').click(function() {
+    $('#openLogin').removeClass('active');
+    $('#openSignUp').addClass('active');
+  });
+
+
+
+  
+	for (var i = 0; i < lastPremier.length; i++) {
+		var moviesData = lastPremier[i];
+		getMovies(moviesData);
+	}
 
 });
 
