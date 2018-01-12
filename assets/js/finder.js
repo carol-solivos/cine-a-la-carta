21075ca1ed2ac6c6c3f889e54b6dcfe383325cb9
naoangel <es.veneros@gmail.com>
2018-01-12 04:15:24 -0300

    cambios updates y más

--- assets/js/finder.js
+++ assets/js/finder.js
@@ -1,11 +1,24 @@
-$(document).ready(() =>{
-
+$(document).ready(function(){
+ /*
+*Función que inicia el carrousel
+*/
+$('.carousel').carousel();
 
 
 /*
 *función que permite funcionar a los select como  tal.
 */
-
 $('select').material_select();
-})
+
+});
+
+
+
+
+
+
+
+      
+
+
 
